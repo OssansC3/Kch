079da41998a34b2cb26ebf3daaa84943fbfe5c76
r-kou <ajiman.misosiru@gmail.com>
2014-10-17 17:48:18 +0900

    GetTweet作成

--- src/kch/GetTweet.java
+++ src/kch/GetTweet.java
@@ -1,7 +1,12 @@
 package kch;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
+
 import twitter4j.*;
+import twitter4j.conf.ConfigurationBuilder;
 
 /**
  * ついーとを取得する
@@ -11,23 +16,48 @@ import twitter4j.*;
  */
 public class GetTweet {
 
+	private static final String key = "";
+	private static final String secret = "";
+	private static final String token = "";
+	private static final String tokenSecret = "";
+	private static TwitterFactory tf;
+
+	public GetTweet(){
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+		cb.setDebugEnabled(true)
+		  .setOAuthConsumerKey(key)
+		  .setOAuthConsumerSecret(secret)
+		  .setOAuthAccessToken(token)
+		  .setOAuthAccessTokenSecret(tokenSecret);
+		tf = new TwitterFactory(cb.build());
+	}
+
 	/**
 	 * ツイート内容を取得するapi
 	 * @param アドレス
 	 * @return ツイート内容
 	 */
-	public void getTweet() {
+	public String getTweet(String userId) {
+		File outFile = new File("tweet.txt");
 		try{
-			Twitter twitter = new TwitterFactory().getInstance();
-			User user = twitter.verifyCredentials();
-			List<Status> statues = twitter.getHomeTimeline();
-			System.out.println("show @"+user.getScreenName()+" shows");
-			for (Status status:statues) {
-			System.out.println("@"+status.getUser().getScreenName()+":"+status.getText());
+			FileWriter out = new FileWriter(outFile);
+			Twitter twitter = tf.getInstance();
+			List<Status> statusList = twitter.getUserTimeline(userId, new Paging(1,100));
+			for(Status status:statusList){
+				out.write(format(status.getText())+"\n");
 			}
-			} catch (TwitterException e) {
-			e.printStackTrace();
-			System.out.println(""+e.getMessage());
-		}	}
+			out.close();
+			return "done.";
+		} catch(TwitterException e){
+			System.err.println("TwitterAPIError:"+e);
+			return "TwitterAPIError:"+e;
+		} catch (IOException e) {
+			System.err.println("FileOutputError:"+e);
+			return "FileOutputError:"+e;
+		}
+	}
 
+	private String format(String str){
+		return str.replaceAll(" ", "").replaceAll("\n", "").replaceAll("\t", "");
+	}
 }
