7b994217bd18473f52e7e59d68c27c496a9cb702
r-kou <ajiman.misosiru@gmail.com>
2014-10-20 21:20:38 +0900

    SignUpAccountの動作確認しました。

--- WebContent/js/signup.js
+++ WebContent/js/signup.js
@@ -1,4 +1,4 @@
-var endpoint = 'http://localhost:50080/axis2/services/SignUpAccount';
+var endpoint = 'http://localhost:8080/axis2/services/SignUpAccount';
 
 $('#signup').click(function() {
 	var userId = $('#userId').val();
@@ -10,13 +10,22 @@ $('#signup').click(function() {
 			userId: userId,
 		},
 		success: function(xml) {
-			switch($('return',xml))
+			switch($('return',xml).text())
 			{
-				case 0:
+				case '0':
+					$('#message').text('アカウント"'+userId+'"を登録しました。');
 					break;
-				case 1:
+				case '1':
+					$('#message').text('アカウント"'+userId+'"は既に登録済みです。');
 					break;
-				case 2:
+				case '2':
+					$('#message').text('アカウント"'+userId+'"はTwitter上に存在しません。');
+					break;
+				case '3':
+					$('#message').text('データベース上でエラーが発生しました。');
+					break;
+				default:
+					$('#message').text('予期せぬエラーが発生しました。');
 					break;
 			}
 		},
