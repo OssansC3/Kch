e85f0018016bf9458f0bcb9896fbd10232966ecd
r-kou <ajiman.misosiru@gmail.com>
2014-10-24 00:42:44 +0900

    getKch get userId from URL.

--- WebContent/js/getKch.js
+++ WebContent/js/getKch.js
@@ -1,16 +1,31 @@
 var endpoint = 'http://localhost:8080/axis2/services/GetKchService';
 
 $(function() {
-	var userId = $('#userId').val();
-
+	var userId = getUrlVars()["userId"];
+	if (userId == "undefined") {
+		userId = "";
+	}
+	$('#message').text("アカウント" + userId);
 	$.ajax({
-		type: 'GET',
-		url: endpoint + '/GetKchImageURI',
-		data: {
-			userId: userId,
+		type : 'GET',
+		async: false,
+		url : endpoint + '/GetKchImageURI',
+		data : {
+			userId : userId,
 		},
-		success: function(xml) {
-			$('#KchScore').text($('return',xml).text());
+		success : function(xml) {
+			$('#KchScore').text($('return', xml).text());
 		},
-  });
+	});
 });
+
+function getUrlVars() {
+	var vars = [], hash;
+	var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+	for (var i = 0; i < hashes.length; i++) {
+		hash = hashes[i].split('=');
+		vars.push(hash[0]);
+		vars[hash[0]] = hash[1];
+	}
+	return vars;
+}
