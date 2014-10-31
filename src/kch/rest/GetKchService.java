3f90ca5287df11238be52fad64ce3468063d58c1
r-kou <ajiman.misosiru@gmail.com>
2014-10-31 12:40:41 +0900

    service modified.

--- src/kch/rest/GetKchService.java
+++ src/kch/rest/GetKchService.java
@@ -1,24 +1,13 @@
 package kch.rest;
 
 import java.util.List;
-import java.util.logging.Logger;
 
 import kch.model.AccountModel;
-import kch.utils.MongoDBUtils;
-
-import com.mongodb.MongoException;
 
 public class GetKchService {
-	//暫定的に画像パスを設定
-	private String ERROR_IMAGE_URI="/images/error.jpg";
-	private String GREEN_IMAGE_URI="/images/green.jpg";
-	private String YELLOW_IMAGE_URI="/images/yellow.jpg";
-	private String RED_IMAGE_URI="/images/red.jpg";
 	private AccountModel am;
-	private Logger logger;
 
 	public GetKchService(){
-		logger = Logger.getLogger(getClass().getName());
 		this.am = new AccountModel();
 	}
 
@@ -41,38 +30,6 @@ public class GetKchService {
 	public int getTotalScore(String userId){
 		return am.getTotalScore(userId);
 	}
-
-	public String GetKchImageURI(String userId){
-		logger.info("GetKchService.getKchImageURI");
-		if(userId==null){
-			return "null";
-		}
-		userId = MongoDBUtils.sanitize(userId);
-		AccountModel account = new AccountModel();
-		if(!account.isRegistered(userId)){
-			return ERROR_IMAGE_URI;
-		}
-		int score = 0;
-		try{
-			score = account.getTotalScore(userId);
-		}
-		catch(MongoException e){
-			return RED_IMAGE_URI;
-		}
-
-		//scoreから3段階評価への対応をとり、URIを投げる
-		//現状適当
-		if(score > 70){
-			return GREEN_IMAGE_URI;
-		}
-		else if (score > 30){
-			return YELLOW_IMAGE_URI;
-		}
-		else{
-			return RED_IMAGE_URI;
-		}
-	}
-
 
 	private int[] toArray(List<Integer> list){
 		int[] array = new int[list.size()];
